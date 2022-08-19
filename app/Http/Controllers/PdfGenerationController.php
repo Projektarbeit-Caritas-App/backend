a239d10a2ac441a23bff0c78bf5e14baef294ef0
René Neeb <rene.neeb@student.fhws.de>
2022-08-19 17:38:38 +0200

    📝 PdfGenerationController documented

--- app/Http/Controllers/PdfGenerationController.php
+++ app/Http/Controllers/PdfGenerationController.php
@@ -6,8 +6,23 @@ use App\Models\Card;
 use Illuminate\Http\Request;
 use Barryvdh\Snappy\Facades\SnappyPdf;
 
+/**
+ * @group PDF
+ * @authenticated
+ */
 class PdfGenerationController extends Controller
 {
+    /**
+     * Print specified Card
+     *
+     * @queryParam "" Base64 encoded PDF No-example
+     * @queryParam download download generated PDF No-example
+     * @queryParam raw inline generated PDF (show in browser) No-example
+     *
+     * @param Request $request
+     * @param Card $card
+     * @return \Illuminate\Contracts\Foundation\Application|\Illuminate\Contracts\Routing\ResponseFactory|\Illuminate\Http\Response
+     */
     public function printCard(Request $request, Card $card)
     {
         $pdfCard = SnappyPdf::loadView('pdf.card', [
