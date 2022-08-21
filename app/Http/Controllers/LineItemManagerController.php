18dcd744cf64c8227681b9427a312d688ccd5ca0
René Neeb <rene.neeb@student.fhws.de>
2022-08-21 15:38:06 +0200

    📝 example response for LineItem

--- app/Http/Controllers/LineItemManagerController.php
+++ app/Http/Controllers/LineItemManagerController.php
@@ -14,6 +14,27 @@ class LineItemManagerController extends Controller
     /**
      * List all LineItems
      *
+     * @response status=200
+     * [
+     *      {
+     *           "id": 6,
+     *           "visit_id": 5,
+     *           "person_id": 1,
+     *           "product_type_id": 1,
+     *           "created_at": "2022-08-16T16:32:23.000000Z",
+     *           "updated_at": "2022-08-16T16:32:23.000000Z"
+     *      },
+     *      {
+     *           "id": 7,
+     *           "visit_id": 6,
+     *           "person_id": 1,
+     *           "product_type_id": 1,
+     *           "created_at": "2022-08-16T16:32:52.000000Z",
+     *           "updated_at": "2022-08-16T16:32:52.000000Z"
+     *      },
+     * ]
+     *
+     *
      * @return \Illuminate\Database\Eloquent\Collection
      */
     public function index()
@@ -34,6 +55,16 @@ class LineItemManagerController extends Controller
 
     /**
      * Show specified LineItem
+     *
+     * @response status=200
+     * {
+     *      "id": 6,
+     *      "visit_id": 5,
+     *      "person_id": 1,
+     *      "product_type_id": 1,
+     *      "created_at": "2022-08-16T16:32:23.000000Z",
+     *      "updated_at": "2022-08-16T16:32:23.000000Z"
+     * }
      *
      * @param \App\Models\LineItem $lineItem
      * @return \App\Models\LineItem
@@ -59,6 +90,11 @@ class LineItemManagerController extends Controller
 
     /**
      * Delete specified LineItem
+     *
+     * @response status=200
+     * {
+     *  "success": true
+     * }
      *
      * @param \App\Models\LineItem $lineItem
      * @return array
