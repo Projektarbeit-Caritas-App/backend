204fd63fe5fbec4a66ddd7929607e026bb78eaaf
René <rene@wolfshoehle.eu>
2022-09-08 17:41:54 +0200

    🩹 Fixing some tests

--- tests/Feature/CheckoutTest.php
+++ tests/Feature/CheckoutTest.php
@@ -14,7 +14,6 @@ use Carbon\Carbon;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Support\Facades\Hash;
 use Tests\TestCase;
-use function PHPUnit\Framework\assertEquals;
 
 class CheckoutTest extends TestCase
 {
@@ -39,122 +38,122 @@ class CheckoutTest extends TestCase
     {
         parent::setUp();
 
-        $this->instance = new Instance;
-        $this->instance->name = "Instance Name";
-        $this->instance->street = "Instance Street";
+        $this->instance           = new Instance;
+        $this->instance->name     = "Instance Name";
+        $this->instance->street   = "Instance Street";
         $this->instance->postcode = "12345";
-        $this->instance->city = "Instance city";
-        $this->instance->contact = "Instance contact";
+        $this->instance->city     = "Instance city";
+        $this->instance->contact  = "Instance contact";
         $this->instance->save();
 
-        $this->organization = new Organization;
-        $this->organization->name = "Organization Name";
-        $this->organization->street = "Organization Street";
+        $this->organization           = new Organization;
+        $this->organization->name     = "Organization Name";
+        $this->organization->street   = "Organization Street";
         $this->organization->postcode = "12345";
-        $this->organization->city = "Organization City";
-        $this->organization->contact = "Organization Contact";
+        $this->organization->city     = "Organization City";
+        $this->organization->contact  = "Organization Contact";
         $this->instance->organizations()->save($this->organization);
 
-        $this->user = new User;
-        $this->user->name = "Test User Name";
-        $this->user->email = "test@web.de";
+        $this->user           = new User;
+        $this->user->name     = "Test User Name";
+        $this->user->email    = "test@web.de";
         $this->user->password = Hash::make("Test User Password");
         $this->user->syncRoles("instance_manager");
         $this->user->instance_id = $this->instance->id;
         $this->organization->users()->save($this->user);
 
-        $this->card = new Card;
+        $this->card              = new Card;
         $this->card->instance_id = $this->instance->id;
-        $this->card->creator_id = $this->user->id;
-        $this->card->last_name = "Kitsune";
-        $this->card->first_name = "Yasu";
-        $this->card->street = "Foxstreet 10";
-        $this->card->postcode = "12345";
-        $this->card->city = "Foxhole";
-        $this->card->comment = "Comment";
-        $this->card->valid_from = Carbon::yesterday();
+        $this->card->creator_id  = $this->user->id;
+        $this->card->last_name   = "Kitsune";
+        $this->card->first_name  = "Yasu";
+        $this->card->street      = "Foxstreet 10";
+        $this->card->postcode    = "12345";
+        $this->card->city        = "Foxhole";
+        $this->card->comment     = "Comment";
+        $this->card->valid_from  = Carbon::yesterday();
         $this->card->valid_until = Carbon::tomorrow();
         $this->card->save();
 
-        $this->person = new Person();
+        $this->person              = new Person();
         $this->person->instance_id = $this->instance->id;
-        $this->person->card_id = $this->card->id;
-        $this->person->gender = "male";
-        $this->person->age = 18;
+        $this->person->card_id     = $this->card->id;
+        $this->person->gender      = "male";
+        $this->person->age         = 18;
         $this->person->save();
 
-        $this->limitationSet = new LimitationSet();
+        $this->limitationSet              = new LimitationSet();
         $this->limitationSet->instance_id = $this->instance->id;
-        $this->limitationSet->name = "A set to limit them all ~ Tolkien II";
-        $this->limitationSet->valid_from = "2022-08-04 12:00:00";
+        $this->limitationSet->name        = "A set to limit them all ~ Tolkien II";
+        $this->limitationSet->valid_from  = "2022-08-04 12:00:00";
         $this->limitationSet->valid_until = "2022-08-04 12:00:00";
         $this->person->limitationSets()->save($this->limitationSet);
 
-        $this->card2 = new Card;
+        $this->card2              = new Card;
         $this->card2->instance_id = $this->instance->id;
-        $this->card2->creator_id = $this->user->id;
-        $this->card2->last_name = "Kitsune";
-        $this->card2->first_name = "Yasu";
-        $this->card2->street = "Foxstreet 10";
-        $this->card2->postcode = "12345";
-        $this->card2->city = "Foxhole";
-        $this->card2->comment = "Comment";
-        $this->card2->valid_from = "2022-07-04 12:00:00";
+        $this->card2->creator_id  = $this->user->id;
+        $this->card2->last_name   = "Kitsune";
+        $this->card2->first_name  = "Yasu";
+        $this->card2->street      = "Foxstreet 10";
+        $this->card2->postcode    = "12345";
+        $this->card2->city        = "Foxhole";
+        $this->card2->comment     = "Comment";
+        $this->card2->valid_from  = "2022-07-04 12:00:00";
         $this->card2->valid_until = "2022-07-04 12:00:00";
         $this->card2->save();
 
-        $this->person2 = new Person();
+        $this->person2              = new Person();
         $this->person2->instance_id = $this->instance->id;
-        $this->person2->card_id = $this->card->id;
-        $this->person2->gender = "male";
-        $this->person2->age = 18;
+        $this->person2->card_id     = $this->card->id;
+        $this->person2->gender      = "male";
+        $this->person2->age         = 18;
         $this->person2->save();
 
-        $this->limitationSet2 = new LimitationSet();
+        $this->limitationSet2              = new LimitationSet();
         $this->limitationSet2->instance_id = $this->instance->id;
-        $this->limitationSet2->name = "A set to limit them all ~ Tolkien II";
-        $this->limitationSet2->valid_from = "2022-08-04 12:00:00";
+        $this->limitationSet2->name        = "A set to limit them all ~ Tolkien II";
+        $this->limitationSet2->valid_from  = "2022-08-04 12:00:00";
         $this->limitationSet2->valid_until = "2022-08-04 12:00:00";
         $this->person2->limitationSets()->save($this->limitationSet2);
 
-        $this->card3 = new Card;
+        $this->card3              = new Card;
         $this->card3->instance_id = $this->instance->id;
-        $this->card3->creator_id = $this->user->id;
-        $this->card3->last_name = "Kitsune";
-        $this->card3->first_name = "Yasu";
-        $this->card3->street = "Foxstreet 10";
-        $this->card3->postcode = "12345";
-        $this->card3->city = "Foxhole";
-        $this->card3->comment = "Comment";
-        $this->card3->valid_from = Carbon::tomorrow();
+        $this->card3->creator_id  = $this->user->id;
+        $this->card3->last_name   = "Kitsune";
+        $this->card3->first_name  = "Yasu";
+        $this->card3->street      = "Foxstreet 10";
+        $this->card3->postcode    = "12345";
+        $this->card3->city        = "Foxhole";
+        $this->card3->comment     = "Comment";
+        $this->card3->valid_from  = Carbon::tomorrow();
         $this->card3->valid_until = Carbon::tomorrow();
         $this->card3->save();
 
-        $this->person3 = new Person();
+        $this->person3              = new Person();
         $this->person3->instance_id = $this->instance->id;
-        $this->person3->card_id = $this->card->id;
-        $this->person3->gender = "male";
-        $this->person3->age = 18;
+        $this->person3->card_id     = $this->card->id;
+        $this->person3->gender      = "male";
+        $this->person3->age         = 18;
         $this->person3->save();
 
-        $this->limitationSet3 = new LimitationSet();
+        $this->limitationSet3              = new LimitationSet();
         $this->limitationSet3->instance_id = $this->instance->id;
-        $this->limitationSet3->name = "A set to limit them all ~ Tolkien II";
-        $this->limitationSet3->valid_from = "2022-08-04 12:00:00";
+        $this->limitationSet3->name        = "A set to limit them all ~ Tolkien II";
+        $this->limitationSet3->valid_from  = "2022-08-04 12:00:00";
         $this->limitationSet3->valid_until = "2022-08-04 12:00:00";
         $this->person3->limitationSets()->save($this->limitationSet3);
 
-        $this->productType = new ProductType();
+        $this->productType              = new ProductType();
         $this->productType->instance_id = $this->instance->id;
-        $this->productType->name = "name";
-        $this->productType->icon = "icon";
+        $this->productType->name        = "name";
+        $this->productType->icon        = "icon";
         $this->productType->save();
 
-        $this->limitation = new Limitation();
-        $this->limitation->instance_id = $this->instance->id;
-        $this->limitation->product_type_id = $this->productType->id;
+        $this->limitation                    = new Limitation();
+        $this->limitation->instance_id       = $this->instance->id;
+        $this->limitation->product_type_id   = $this->productType->id;
         $this->limitation->limitation_set_id = $this->limitationSet->id;
-        $this->limitation->limit = 5;
+        $this->limitation->limit             = 5;
         $this->limitation->save();
     }
 
@@ -238,7 +237,6 @@ class CheckoutTest extends TestCase
     public function test_create_checkout()
     {
         $payload = [
-            'comment' => 'Took an additional buggy for his newly born child',
             'lineItems' => [
                 [
                     'person_id' => $this->person->id,
@@ -248,39 +246,8 @@ class CheckoutTest extends TestCase
             ]
         ];
 
-        // dd($payload);
-        /*
-          array:2 [
-          "comment" => "Took an additional buggy for his newly born child"
-          "lineItems" => array:1 [
-            0 => array:3 [
-              "person_id" => 210
-              "product_type_id" => 95
-              "amount" => 5
-            ]
-           ]
-         ]
-         */
-
         $response = $this->actingAs($this->user)
-            ->post('/api/card/visit/' . $this->card->id, $payload);
-        // ->assertSuccessful();
-
-        dd($response->exception); //FIXME
-
-        /*
-            #message: "foreach() argument must be of type array|object, null given"
-            #code: 0
-            #file: "./app/Http/Controllers/CheckoutController.php"
-            #line: 191
-            #severity: E_WARNING
-            trace: {
-              ./app/Http/Controllers/CheckoutController.php:191 {
-                Illuminate\Foundation\Bootstrap\HandleExceptions->handleError($level, $message, $file = '', $line = 0, $context = [])^
-                ›
-                › foreach ($items as $lineItem) {
-                ›     $amount = $lineItem['amount'];
-              }
-         */
+            ->post('/api/card/visit/' . $this->card->id, $payload)
+            ->assertSuccessful();
     }
 }
--- tests/Feature/InstanceTest.php
+++ tests/Feature/InstanceTest.php
@@ -20,22 +20,22 @@ class InstanceTest extends TestCase
      */
     public function test_create_instance_command()
     {
-       $this->artisan('instance:create')
-           ->expectsOutput('Details for the new instance:')
-           ->expectsQuestion('Name', 'Test Instance Name')
-           ->expectsQuestion('Street', 'Test Instance Street')
-           ->expectsQuestion('Postcode', 'Test Instance Postcode')
-           ->expectsQuestion('City', 'Test Instance City')
-           ->expectsQuestion('Contact', 'Test Instance Contact')
-           ->expectsOutput('Details for the admin user of the instance:')
-           ->expectsQuestion('Name', 'Test User Name')
-           ->expectsQuestion('E-Mail', 'test@web.de')
-           ->expectsQuestion('Password', 'Test User Password')
-           ->expectsQuestion('Confirm Password', 'Test User Password')
-           ->expectsOutput('Instance created')
-           ->expectsOutput('Organization created')
-           ->expectsOutput('User created')
-           ->assertSuccessful();
+        $this->artisan('instance:create')
+            ->expectsOutput('Details for the new instance:')
+            ->expectsQuestion('Name', 'Test Instance Name')
+            ->expectsQuestion('Street', 'Test Instance Street')
+            ->expectsQuestion('Postcode', 'Test Instance Postcode')
+            ->expectsQuestion('City', 'Test Instance City')
+            ->expectsQuestion('Contact', 'Test Instance Contact')
+            ->expectsOutput('Details for the admin user of the instance:')
+            ->expectsQuestion('Name', 'Test User Name')
+            ->expectsQuestion('E-Mail', 'test@web.de')
+            ->expectsQuestion('Password', 'Test User Password')
+            ->expectsQuestion('Confirm Password', 'Test User Password')
+            ->expectsOutput('Instance created')
+            ->expectsOutput('Organization created')
+            ->expectsOutput('User created')
+            ->assertSuccessful();
     }
 
     /**
@@ -45,33 +45,35 @@ class InstanceTest extends TestCase
      */
     public function test_delete_instance_command() //FIXME Not enough arguments (missing: "instance")
     {
-        $instance = new Instance;
-        $instance->name = "Instance Name";
-        $instance->street = "Instance Street";
-        $instance->postcode = "12345";
-        $instance->city = "Instance city";
-        $instance->contact = "Instance contact";
+        $instance             = new Instance;
+        $instance->name       = "Instance Name";
+        $instance->street     = "Instance Street";
+        $instance->postcode   = "12345";
+        $instance->city       = "Instance city";
+        $instance->contact    = "Instance contact";
         $instance->created_at = "2022-07-04 12:00:00";
         $instance->updated_at = "2022-07-04 12:00:00";
         $instance->save();
 
-        $organization = new Organization;
-        $organization->name = "Organization Name";
-        $organization->street = "Organization Street";
+        $organization           = new Organization;
+        $organization->name     = "Organization Name";
+        $organization->street   = "Organization Street";
         $organization->postcode = "12345";
-        $organization->city = "Organization City";
-        $organization->contact = "Organization Contact";
+        $organization->city     = "Organization City";
+        $organization->contact  = "Organization Contact";
         $instance->organizations()->save($organization);
 
-        $user = new User;
-        $user->name = "Test User Name";
-        $user->email = "test@web.de";
+        $user           = new User;
+        $user->name     = "Test User Name";
+        $user->email    = "test@web.de";
         $user->password = Hash::make("Test User Password");
         $user->syncRoles("instance_manager");
         $user->instance_id = $instance->id;
         $organization->users()->save($user);
 
-        $this->artisan('instance:delete ' . $instance->id)
+        $instance->loadCount(['organizations', 'users']);
+
+        $this->artisan('instance:delete', ['instance' => $instance->id])
             ->expectsOutput('ID: ' . $instance->id)
             ->expectsOutput('Name: ' . $instance->name)
             ->expectsOutput('Street: ' . $instance->street)
@@ -80,8 +82,17 @@ class InstanceTest extends TestCase
             ->expectsOutput('Contact: ' . $instance->contact)
             ->expectsOutput('Created: ' . $instance->created_at->format('G:i \o\n l jS F Y'))
             ->expectsOutput('Updated: ' . $instance->updated_at->format('G:i \o\n l jS F Y'))
+            ->expectsOutput(sprintf(
+                'This action will delete %s organizations with %s users.',
+                $instance->organizations_count,
+                $instance->users_count
+            ))
             ->expectsConfirmation('Do you really want to delete the instance?', 'yes')
             ->expectsOutput('Instance deleted')
             ->assertSuccessful();
+
+        $this->assertModelMissing($instance)
+            ->assertModelMissing($organization)
+            ->assertModelMissing($user);
     }
 }
